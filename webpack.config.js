4146d5096e702887dd307974b1e97b8671a309d6
Zachary McCain <zacmmcca@gmail.com>
2021-03-06 19:18:05 -0600

    Updated boilerplate code.

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+module.exports = {
+  entry: path.resolve(__dirname, 'client/index.js'),
+  watch: true,
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'public/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'client'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: 'defaults'
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      }
+    ]
+  }
+}
