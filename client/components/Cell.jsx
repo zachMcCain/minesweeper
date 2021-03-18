35914201bb2856fb72398f56ece085cc8db403fa
Zachary McCain <zacmmcca@gmail.com>
2021-03-17 19:30:04 -0500

    Updated to cover unselected elements.

--- client/components/Cell.jsx
+++ client/components/Cell.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Cell = ({ val, select, x, y }) => {
+  const [covered, uncover] = useState(true)
   if (val === false) {
     val = <span className="covered" onClick={select}>&#128163;</span>
+  }
+
+  if (covered) {
+    return <td onClick={() => {
+      select(x, y);
+      uncover(false);
+    }} className="covered" id={`${x}${y}`}></td>
   }
   // Cell {val}
   return <td onClick={() => {
